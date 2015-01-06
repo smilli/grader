2627974c10860264673e48f686299df1f1fb0cbe
Smitha Milli <smitha.milli@gmail.com>
2015-01-06 17:43:16 -0600

    Word correcting

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,16 +1,46 @@
+var assignments = [
+      'This is a student\'s assignment',
+      'This is another student\'s assignment'
+];
+
+
+var getAssignments = function(data) {
+    assignments = []
+    for (var i = 0; i < data.length; i++) {
+      assignment = []
+      words = data[i].split(' ');
+      for (var j = 0; j < words.length; j++) {
+        word = {'correct': true, 'correction':'', 'selected': false};
+        word.text = words[j];
+        assignment.push(word);
+      }
+      assignments.push(assignment);
+    }
+    return assignments;
+}
+
+
 var app = angular.module('graderApp', [])
   .controller('GraderController', ['$scope', function($scope) {
-    $scope.assignments = [
-      'This is a student\'s assignment'.split(' '),
-      'This is another student\'s assignment'.split(' ')
-    ];
-    
+    $scope.assignments = getAssignments(assignments);
     $scope.focusGradeBox = false;
-    $scope.word = '';
+    $scope.word = null;
+    $scope.correctTo = '';
 
     $scope.wordClicked = function(word) {
-      console.log(word + ' clicked');
+      if ($scope.word) {
+        $scope.word.selected = false;
+      }
       $scope.word = word;
+      $scope.word.selected = true;
       $scope.focusGradeBox = true;
+    }
+
+    $scope.submit = function() {
+      if ($scope.word) {
+        $scope.word.correct = false;
+        $scope.word.correction = $scope.correctTo;
+        $scope.correctTo = '';
+      }
     }
   }])
