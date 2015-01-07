b903d61a8d4c358daeedef726dd9e43e1cde3cae
Smitha Milli <smitha.milli@gmail.com>
2015-01-06 18:40:05 -0600

    Create words on server

--- app/app.py
+++ app/app.py
@@ -1,11 +1,38 @@
-from flask import Flask
-from flask import make_response
+from flask import Flask, jsonify, make_response
+import json
 app = Flask(__name__)
+
+
+def create_word(word_text, correction=None):
+    word = {}
+    word['text'] = word_text
+    if correction:
+        word['correction'] = correction
+        word['correct'] = False
+    else:
+        word['correction'] = ''
+        word['correct'] = True
+    word['selected'] = False
+    return word
 
 
 @app.route('/')
 def main():
     return make_response(open('templates/index.html').read())
+
+
+@app.route('/assignments', methods=['GET', 'POST'])
+def get_assignments():
+    assignments = [
+        'This is a student\'s assignment',
+        'This is another student\'s assignment'
+    ]
+    data = []
+    for assignment in assignments:
+        words = assignment.split()
+        for word in words:
+            data.append(create_word(word))
+    return json.dumps(data)
 
 
 if __name__ == '__main__':
